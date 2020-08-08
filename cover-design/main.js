476a08bca84ae6fb325b463f652204856492b390
haoyu <haoyu953@gmail.com>
2020-08-08 15:57:09 +0800

    优化

--- cover-design/main.js
+++ cover-design/main.js
@@ -12,45 +12,78 @@ for (let i = 1; i <= 10 ; i++) {
 queue.loadManifest(images, true, './assets/');
 
 queue.on("complete", function(){
-    // document.getElementById('loading').remove();
     $('#loading').remove();
-    // document.getElementsByClassName('container')[0].className = 'container';
     $('.container').css('display', 'block');
 
-    // 点击上滑Icon
-    $('#arrowIcon img').on('click', (e) => {
+    // // 点击上滑Icon
+    // $('#arrowIcon img').on('click', (e) => {
+    //     showCatalog();
+    // })
+
     // 封面上滑
-    $('#cover').css('display', 'none');
-    $('#catalog').css('display', 'flex');
-    $('#catalog').css('height', '100%');
-    $('#arrowIcon').css('display', 'none');
-        
-    })
+    let showCatalog = () => {
+        $('#cover').css('display', 'none');
+        $('#catalog').css('display', 'flex');
+        $('#arrowIcon').css('display', 'none');
+    }
+
+    let startX, startY, x, y;
+    $('#arrowIcon').on('touchstart', (e) => {
+        e.preventDefault();
+        const touch = e.touches[0];
+        startX = touch.pageX;
+        startY = touch.pageY;
+    });
+    $('#arrowIcon').on('touchmove', (e) => {
+        e.preventDefault();
+        const touch = e.touches[0];
+        x = startX - touch.pageX;
+        y = startY - touch.pageY;
+    });
+    $('#arrowIcon').on('touchend', (e) => {
+        if (y > 0) {
+            console.log('上滑：' + y + 'px');
+            showCatalog();
+        }
+        e.preventDefault();
+    });
     
     // 点击目录项
-    $('.catalogItem').on('click', (e) => {
-        const name = e.currentTarget.getAttribute('link');
+    $('#catalog .row img').on('click', (e) => {
+        const index = e.currentTarget.getAttribute('index');
         setTimeout(() => {
             $('#catalogIcon').css('display', 'block');
         }, 500);
         
         $('#catalog').css('display', 'none');
-        $('#content img').attr('src', './assets/' + name);
+        $('#content img').attr('src', './assets/' + '书籍设计' + index + '.jpg');
         $('#content').scrollTop(0);
         $('#content').css('display', 'block');
-
+        
+        
     })
-
+    
     // 点击目录Icon
     $('#catalogIcon').on('click', () => {
-        $('#closeIcon').css('display', 'block');
+        $('#catalog').css('background-image', "url('./assets/目录切图/目录背景透明.png')")
         $('#catalog').css('display', 'flex');
+        $('#closeIcon').css('display', 'block');
         $('#catalog').css('position', 'absolute');
         $('#catalog').css('background-color', 'rgba(0, 0, 0, 0.9)');
+        
     })
 
     // 点击目录closeIcon
     $('#closeIcon').on('click', () => {
         $('#catalog').css('display', 'none');
     })
+    
+    // new iScroll('#content', {
+    //     onRefresh: () => {},
+    //     onScrollMove: () => {},
+    //     onScrollEnd: () => {
+    //         console.log(this.y);
+    //     }
+    // });
+    
 })
