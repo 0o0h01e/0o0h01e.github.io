52e312f40557e1bba0047d6b6c09eb831762a753
haoyu <haoyu953@gmail.com>
2020-08-10 23:58:11 +0800

    微博图床

--- cover-design/main.js
+++ cover-design/main.js
@@ -1,20 +1,50 @@
 const queue = new createjs.LoadQueue(false);
 
-const images = [];
-for (let i = 1; i <= 10 ; i++) {
-    images.push('书籍设计' + i + '.jpg');    
+const images = [
+    '封面.jpg',
+    'arrow-up.png',
+    '目录切图/目录背景.jpg',
+    '目录切图/目录背景透明.png',
+];
+
+const contentImgs = [
+    'https://ww1.sinaimg.cn/large/a1823812gy1ghm5879dntj20ku3r6qhr.jpg',
+    'https://ww1.sinaimg.cn/large/a1823812gy1ghm5bu2xqlj20ku1rvjyi.jpg',
+    'https://ww1.sinaimg.cn/large/a1823812gy1ghm5cvw3lsj20ku2osn69.jpg',
+    'https://ww1.sinaimg.cn/large/a1823812gy1ghm5d9qs84j20ku2cek28.jpg',
+    'https://ww1.sinaimg.cn/large/a1823812gy1ghm5dheddij20ku2hn7g2.jpg',
+    'https://ww1.sinaimg.cn/large/a1823812gy1ghm5drbv2ij20ku284tgd.jpg',
+    'https://ww1.sinaimg.cn/large/a1823812gy1ghm5e0x73tj20ku2kgn89.jpg',
+    'https://ww1.sinaimg.cn/large/a1823812gy1ghm5e8cvx7g20ku1i076r.jpg',
+    'https://ww1.sinaimg.cn/large/a1823812gy1ghm5efabw6g20ku1vqn05.jpg',
+    'https://ww1.sinaimg.cn/large/a1823812gy1ghm5ew7vvbj20ku4fc7l5.jpg',
+];
+
+for (let i = 1; i <= 10; i++) {
+    images.push('目录切图/目录_' + (i < 10 ? '0' + i : i) + '.jpg');
 }
+
+// for (let i = 1; i <= 10 ; i++) {
+//     images.push('书籍设计' + i + '.jpg');    
+// }
+for (let i = 0; i < contentImgs.length; i++) {  
+    images.push(contentImgs[i]);
+    
+}
+
+console.log(images);
+
 queue.loadManifest(images, true, './assets/');
 
-queue.on("complete", function(){
-    $('#loading').remove();
-    $('.container').css('display', 'block');
+queue.on('progress', progress => {
+    if (progress.progress > 0.7) {
+        $('#loading').remove();
+        $('.container').css('display', 'block'); 
+    }
+})
+// queue.on("complete", function(){});
 
-    // // 点击上滑Icon
-    // $('#arrowIcon img').on('click', (e) => {
-    //     showCatalog();
-    // })
-
+$(document).ready(function () {
     // 封面上滑
     let showCatalog = () => {
         $('#cover').css('display', 'none');
@@ -53,7 +83,8 @@ queue.on("complete", function(){
         }, 500);
         
         $('#catalog').css('display', 'none');
-        $('#content img').attr('src', './assets/' + '书籍设计' + index + '.jpg');
+        // $('#content img').attr('src', './assets/' + '书籍设计' + index + '.jpg');
+        $('#content img').attr('src', contentImgs[index - 1]);
         $('#showContainer').scrollTop(0);
         $('#content').css('display', 'block');
 
@@ -92,10 +123,7 @@ queue.on("complete", function(){
         myScroll = new iScroll('content', {
             useTransition: true,
             vScrollbar: false,
-            // deceleration: 0.01,
             onScrollMove: function() {
-                console.log(this.y);
-
                 if (this.y < 0) {
                     dragUpY = this.y - this.maxScrollY;
                     if (dragUpY <= -100) {
@@ -121,13 +149,8 @@ queue.on("complete", function(){
                         $('.dragTip').css('display', 'none');
                     }
                 }
-
-                // dragY = d;
-                console.log('Scrolling dragDownY and dragUpY: ', dragDownY, dragUpY);
             }, 
             onScrollEnd: () => {
-                console.log('Scrolling dragDownY and dragUpY: ', dragDownY, dragUpY);
-
                 // 上滑重新加载
                 if (dragUpY <= -100) {
                     dragUpY = 0;    // 重置
@@ -136,7 +159,8 @@ queue.on("complete", function(){
                         
                         index++;
                         console.log(index);
-                        $('#content img').attr('src', './assets/' + '书籍设计' + index + '.jpg');
+                        // $('#content img').attr('src', './assets/' + '书籍设计' + index + '.jpg');
+                        $('#content img').attr('src', contentImgs[index - 1]);
                         myScroll.scrollTo(0, 0);
                         
                         setTimeout(() => {
@@ -154,7 +178,8 @@ queue.on("complete", function(){
 
                         index--;
                         console.log(index);
-                        $('#content img').attr('src', './assets/' + '书籍设计' + index + '.jpg');
+                        // $('#content img').attr('src', './assets/' + '书籍设计' + index + '.jpg');
+                        $('#content img').attr('src', contentImgs[index - 1]);
                         myScroll.scrollTo(0, 0);
                         
                         setTimeout(() => {
@@ -165,5 +190,5 @@ queue.on("complete", function(){
                 }
             }
         });
-    }
+    } 
 });
